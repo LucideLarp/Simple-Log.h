3f506ef4fb905daa2e0d611ff0b3e59173b59df7
LARP <lucidee@proton.me>
2026-06-08 17:52:38 +0200

    Optmized and shit

--- Log.h
+++ Log.h
@@ -1,14 +1,6 @@
 #pragma once
 #include <iostream>
-#include <ostream>
-#include <string>
 
-
-inline std::string input;
-auto Log(const auto& message) {
-    std::cout << message << std::endl;
-}
-
-void Read() {
-    std::getline(std::cin, input);
+void Log(const auto& message) {
+    std::cout << message;
 }
